9c49138d1d64c955eef590f424ff0742d736c5aa
securelyshare-swathi-bali <bali.swathi@securelyshare.com>
2022-10-19 22:58:07 +0530

    search  functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,6 +9,7 @@ const Main = () => {
   const [url, setUrl] = useState(
     "https://gateway.marvel.com:443/v1/public/characters?limit=30&ts=1&apikey=a972fb1c42f56020625d88f70bd403f5&hash=ac01de495062c916cc7137c12360b6cb"
   );
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -20,14 +21,27 @@ const Main = () => {
       }
     };
     fetchCharacters();
-  }, []);
+  }, [url]);
+
+  function searchMarvelCharacters() {
+    setUrl(
+      `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${searchInput}&ts=1&apikey=a972fb1c42f56020625d88f70bd403f5&hash=ac01de495062c916cc7137c12360b6cb`
+    );
+  }
 
   return (
     <>
       <div className="header">
         <div className="search-bar">
           <img src="./images/logo.jpg" alt="logo" />
-          <input type="search" placeholder="Search Here" className="search" />
+          <input
+            type="search"
+            placeholder="Search Here"
+            className="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            onKeyPress={searchMarvelCharacters}
+          />
         </div>
       </div>
       <div className="content">
